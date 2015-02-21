67f32f932397e5aa52a99aac24d03fc145ac5add
Nikita Gusakov <dev@nkt.me>
2015-02-21 13:00:39 +0300

    Fixed Grid tests

--- src/__tests__/Grid.js
+++ src/__tests__/Grid.js
@@ -1,13 +1,24 @@
 jest.dontMock('../Grid');
 
 describe('Grid', function () {
-    it('Should add container class to element', function () {
-        var React = require('react/addons');
-        var Grid = require('../Grid');
-        var TestUtils = React.addons.TestUtils;
-        var assert = require('assert');
+    var React = require('react/addons');
+    var Grid = require('../Grid');
+    var TestUtils = React.addons.TestUtils;
 
+    it('Should add container class to element', function () {
         var grid = TestUtils.renderIntoDocument(<Grid></Grid>);
-        assert.equals('container', grid.getDOMNode().className);
+        expect(grid.getDOMNode().className).toEqual('container');
     });
+
+    it('Should not replace class', function () {
+        var grid = TestUtils.renderIntoDocument(<Grid className="foo"></Grid>);
+        var className = grid.getDOMNode().className;
+        expect(className).toContain('foo');
+        expect(className).toContain('container');
+    });
+
+    it('Should wrap children nodes', function () {
+        // @todo: complete test
+        var grid = TestUtils.renderIntoDocument(<Grid><span>test</span></Grid>);
+    })
 });
